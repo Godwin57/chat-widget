9753a3dc151130dac54ba652315216641d29f40d
Godwin Emeribe <godwinemeribe23@gmail.com>
2024-03-28 14:53:10 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <>
+            <h1 className="text-blue-400 text-3xl font-bold underline text-center">
+                Welcome
+            </h1>
+            <p className="text-3xl text-underline">How are you</p>
+        </>
+    );
+}
+
+export default App;
